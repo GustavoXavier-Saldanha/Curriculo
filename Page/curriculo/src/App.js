4fb8478fb8f6b8599f4524b182b6e3e5ceda139b
Gustavo Xavier <gustavosaldxav@gmail.com>
2021-09-10 08:59:52 -0300

    adição inicial

---
+++ Page/curriculo/src/App.js
@@ -0,0 +1,13 @@
+import NavBar from "./components/navbar";
+import Perfil from "./screen/Perfil";
+
+function App() {
+  return (
+    <div className="App">
+        <NavBar/>
+        <Perfil/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Page/curriculo/src/components/navbar/index.js
@@ -0,0 +1,32 @@
+import './style.css'
+
+const NavBar = () => {
+  
+    return (
+  
+      <nav className="navbar ">
+        
+      
+            <ul >
+                <li>
+                    <a href="#perfil">Pelfil</a>
+                </li>
+                <li>
+                    <a href="#formacaoAcademica">Formação Acadêmica</a>
+                </li>
+                <li>
+                    <a href="#certificacao">Certificações</a>
+                </li>
+                <li>
+                    <a href="#software">Softwares</a>
+                </li>
+                <li>
+                    <a href="#contato">Contato</a>
+                </li>
+            </ul>
+       
+      </nav>
+    )
+  }
+  
+  export default NavBar
